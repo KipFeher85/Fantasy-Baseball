bbfcd0d2e22c34240679b2b372883075dd974566
KipFeher85 <51102746+KipFeher85@users.noreply.github.com>
2021-06-22 12:38:02 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python
-
 from setuptools import setup
-
 
 def readme():
     with open('readMe.rst') as f:
         return f.read()
 
-setup(name='yahoo_fantasy_baseball_stat_analysis',
-      version='0.1',
+setup(name='yahoo_fb_stat_analysis',
+      version='1.4',
       description='Python module to allow for advanced statistical analysis',
       long_description=readme(),
       url='https://github.com/KipFeher85/Fantasy-Baseball',
       author='Kip Feher',
       author_email='kipfeher85@gmail.com',
       license='MIT',
-      packages=['yahoo_fantasy_baseball_stat_analysis'],
+      packages=['yahoo_fb_stat_analysis'],
       classifiers=[
           'License :: OSI Approved :: MIT License',
           'Programming Language :: Python :: 3.7',
       ],
-      install_requires=['yahoo_oauth', 'yahoo_fantasy_api', 'statsapi'],
+      install_requires=['yahoo_oauth', 'yahoo_fantasy_api'],
       zip_safe=False,
       )
